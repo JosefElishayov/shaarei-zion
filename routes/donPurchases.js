ce48d6ecd882d85f5b727c0349510fd5362931ff
JosefElishayov <121160405+JosefElishayov@users.noreply.github.com>
2023-02-02 21:47:39 +0200

    aaa

---
+++ routes/donPurchases.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { auth, authAdmin } = require("../middlewares/auth");
+const { validateDonPurchase, DonPurchaseModel } = require("../models/donPurchasModel");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    res.json({ msg: "Api Work 400" });
+})
+
+router.post("/", auth, async (req, res) => {
+    let validBody = validateDonPurchase(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    try {
+        let purchase = new DonPurchaseModel(req.body);
+        purchase.user_id = req.tokenData._id;
+        purchase.user_name = req.tokenData.name;
+        purchase.token_id="1111";
+        await purchase.save()
+
+        res.status(201).json(purchase);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+router.put("/:id", authAdmin, async (req, res) => {
+    let validBody = validateDonPurchase(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+    try {
+        let id = req.params.id;
+        let data = await DonPurchaseModel.updateOne({ _id: id }, req.body);
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+router.delete("/:id", authAdmin, async (req, res) => {
+    try {
+        let id = req.params.id;
+        let data = await DonPurchaseModel.deleteOne({ _id: id });
+        res.json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(502).json({ err })
+    }
+})
+
+module.exports = router;
